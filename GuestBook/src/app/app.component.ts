e5167fc9102fee453512bd7035eceeec3ac308ac
YauhenHursky <e.gursky@naveksoft.com>
2019-05-31 11:29:32 +0300

    throw error in console

--- GuestBook/src/app/app.component.ts
+++ GuestBook/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { WebsocketService } from "./websocket.service";
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './websocket.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
-  providers: [WebsocketService]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [WebsocketService],
 })
 export class AppComponent implements OnInit {
   constructor(private websocketService: WebsocketService) {}
-  ngOnInit() {}
+  ngOnInit() {
+    let i = 0;
+    setInterval(() => {
+      i++;
+      throw new Error(`${i} OMG!!!`);
+    }, 5000);
+  }
 }
